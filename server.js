342c4b326dbc3d424af846e40c02262f8e3d841b
btmitchell@hotmail.com <btmitchell@hotmail.com>
2020-09-12 01:24:27 +0000

    periodic backup

--- server.js
+++ server.js
@@ -1,34 +1,77 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express    = require('express');
+const bodyParser = require("body-parser");
+const cors       = require('cors');
+const mongoose   = require('mongoose');
 
-var cors = require('cors');
-
-var app = express();
-
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const setupDB = () => {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log('connected');
+  });
+
+  const urlSchema = new mongoose.Schema({  
+    id: Number,
+    url: String  
+  });
+
+  let UrlEntry = mongoose.model('urlShortList', urlSchema);
+  return UrlEntry
+};
+
+const findNextID = () => {
+
+  //find the lowest ID
+    const query = UrlEntry.find()
+                          .sort({ 'id': 'desc' })
+                          .limit(1)
+                          .select('id');
+    query.exec(function(err, data){
+    console.log(data[0].id);    
+    return data[0].id;
+  });        
+}
+
+const UrlEntry = setupDB();
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+// user posts URL here
+app.post("/api/shorturl/new", bodyParser.urlencoded({extended: false}), (req, res, next) => {
+  console.log(req.body.url);
+  console.log(findNextID());
+       
+  
+  //check if it exists  
+  UrlEntry.findOne({"url":req.body.url}, (err, urlFound) =>{
+
+    if(err){
+      console.log(`e:${err}`);
+    } else if (urlFound == null){
+      console.log(`notfound: ${urlFound}`);
+      const newUrl = UrlEntry({"id":2,"url":req.body.url});
+      newUrl.save( (err,data) =>{
+        console.log(`data:${data}`);
+      })
+    }
+    else {
+      console.log(`found:${urlFound}`);
+    };    
+  });
 });
 
 
