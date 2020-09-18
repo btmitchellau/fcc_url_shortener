10317b164cc3bd1694567c14ea610bcbd8d8d3bf
btmitchell@hotmail.com <btmitchell@hotmail.com>
2020-09-18 16:17:48 +0930

    eslint changes

--- server.js
+++ server.js
@@ -1,100 +1,85 @@
-'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const urlExists = require('url-exists');
+const db = require('./db');
 
-const express = require('express'),
-    bodyParser = require("body-parser"),
-    cors = require('cors'),
-    urlExists = require("url-exists"),
-    db = require('./db'),
-    app = express(),
-    port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-
-console.log(db);
 app.use(cors());
 app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use('/public', express.static(process.cwd() + '/public'))
+  extended: false,
+}));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
 // user posts URL here
-app.post("/api/shorturl/new", (req, res) => {
+app.post('/api/shorturl/new', (req, res) => {
+  urlExists(req.body.url, (err, exists) => {
+    if (!exists) {
+      res.json({
+        error: 'invalid URL',
+      });
+    } else {
+      let nextId = 1;
 
-    urlExists(req.body.url, (err, exists) => {
-        if (!exists) {
-            console.log(err);
-            res.json({
-                "error": "invalid URL"
+      // find the lowest ID
+      const query = db.UrlEntry.find()
+        .sort({
+          id: 'desc',
+        })
+        .limit(1)
+        .select('id url');
+
+      query.exec((queryExecErr, data) => {
+        nextId = parseInt(data[0].id) + 1;
+
+        // check if it exists
+        db.UrlEntry.findOne({
+          url: req.body.url,
+        }, (dbFindOneErr, urlFound) => {
+          if (err) {
+
+          } else if (urlFound == null) {
+            const newUrl = db.UrlEntry({
+              id: nextId,
+              url: req.body.url,
             });
-        } else {
-
-            let nextId = 1;
-
-            //find the lowest ID
-            const query = db.UrlEntry.find()
-                .sort({
-                    'id': 'desc'
-                })
-                .limit(1)
-                .select('id url');
-
-            query.exec((err, data) => {
-
-                nextId = parseInt(data[0].id) + 1;
-
-                // check if it exists  
-                db.UrlEntry.findOne({
-                    "url": req.body.url
-                }, (err, urlFound) => {
-
-                    if (err) {
-                        console.log(`e:${err}`);
-
-                    } else if (urlFound == null) {
-                        const newUrl = db.UrlEntry({
-                            "id": nextId,
-                            "url": req.body.url
-                        });
-                        newUrl.save((err, data) => {});
-                    }
-                    res.json({
-                        "original_url": req.body.url,
-                        "short_url": nextId
-                    });
-                });
-            });
-        }
-    });
+            newUrl.save((err, data) => {});
+          }
+          res.json({
+            original_url: req.body.url,
+            short_url: nextId,
+          });
+        });
+      });
+    }
+  });
 });
 
 // user retrieving a URL
 app.get('/:urlId', (req, res) => {
-    console.log(req.params.urlId)
+  const query = db.UrlEntry.where({
+    id: req.params.urlId,
+  });
+  query.findOne((err, url) => {
+    if (err) {
 
-    const query = db.UrlEntry.where({
-        id: req.params.urlId
-    });
-    query.findOne(function(err, url) {
-        if (err) {
-            console.log(err)
-        }
-        if (url) {
-            console.log(url)
-            if (url != null) {
-                console.log('redirecting...')
-                res.redirect(url.url);
-            } else {
-                res.json({
-                    "error": "url lookup failed"
-                });
-            }
-        }
-    });
+    }
+    if (url) {
+      if (url != null) {
+        res.redirect(url.url);
+      } else {
+        res.json({
+          error: 'url lookup failed',
+        });
+      }
+    }
+  });
 });
 
-app.listen(port, () => {
-    console.log('Node.js listening ...');
-});
+app.listen(port);
